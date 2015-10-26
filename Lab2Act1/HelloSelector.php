31aedaf8fc849c9f5c58fd7f3a226dc066f2be77
1206499 <1206499@rgu.ac.uk>
2015-10-26 14:35:02 +0000

    Moved HelloPrinter.php and HelloSelector.php into the Lab2Act1 directory

---
+++ Lab2Act1/HelloSelector.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <p>Select where you would like to go:</p>
+
+        <ul>
+            <li><a href="http://1206499-labwork.azurewebsites.net/Lab2Act1/HelloPrinter.php?planet=Earth">Earth</a></li>
+            <li><a href="http://1206499-labwork.azurewebsites.net/Lab2Act1/HelloPrinter.php?planet=Mars">Mars</a></li>
+            <li><a href="http://1206499-labwork.azurewebsites.net/Lab2Act1/HelloPrinter.php?planet=Uranus">Uranus</a></li>
+        </ul>
+    </body>
+</html>
