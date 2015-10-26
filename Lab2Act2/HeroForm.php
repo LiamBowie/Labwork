25df87c9723512eaf04c5fee0c0c8895b4dc51be
1206499 <1206499@rgu.ac.uk>
2015-10-26 14:46:17 +0000

    Added HelloForm.php

---
+++ Lab2Act2/HeroForm.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form action="HeroResponse" method="post">
+            <label>Forename</label><input type="text" name="forename">
+            <label>Surname</label><input type="text" name="surname">
+            <label>Gender</label><input title="m" type="checkbox" name="male">
+
+        </form>
+    </body>
+</html>
