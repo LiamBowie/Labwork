098388f8b2bb7d7ed9fe363333eb110a993002a1
1206499 <1206499@rgu.ac.uk>
2015-10-26 15:02:25 +0000

    Added Checkboxes

--- Lab2Act2/HeroForm.php
+++ Lab2Act2/HeroForm.php
@@ -3,9 +3,24 @@
         <form action="HeroResponse" method="post">
                 <p><label>Forename</label><input type="text" name="forename"></p>
                 <p><label>Surname</label><input type="text" name="surname"></p>
-                <div><label>Gender</label>
+                <div><label>Gender:</label>
                     <p><input type="radio" value="male" name="gender">Male</p>
                     <p><input type="radio" value="female" name="gender">Female</p>
+                </div>
+                <div>
+                    Superpowers:
+                    <input type="checkbox" name="flight">Filght
+                    <input type="checkbox" name="invisibility">Invisibility
+                    <br>
+                    <input type="checkbox" name="superstrenth">Super Strength
+                    <input type="checkbox" name="superspeed">Super Speed
+                    <br>
+                    <input type="checkbox" name="specific-weapon-skills">Specific Weapon Skills
+                    <input type="checkbox" name="super-rich">Super Rich
+                    <br>
+                    <input type="checkbox" name="other">Other
+                    <label>Please Specify</label><input type="text" name="otherskill">
+                    <br>
                 </div>
         </form>
     </body>
