ec87d38e28e36153ba4c34096b3a81178fcfe360
1206499 <1206499@rgu.ac.uk>
2015-10-26 14:51:18 +0000

    Updated form

--- Lab2Act2/HeroForm.php
+++ Lab2Act2/HeroForm.php
@@ -1,10 +1,13 @@
 <html>
     <body>
         <form action="HeroResponse" method="post">
-            <label>Forename</label><input type="text" name="forename">
-            <label>Surname</label><input type="text" name="surname">
-            <label>Gender</label><input title="m" type="checkbox" name="male">
-
+            <ul>
+                <label>Forename</label><input type="text" name="forename">
+                <label>Surname</label><input type="text" name="surname">
+                <label>Gender</label>
+                <input type="radio" value="male" name="gender">Male
+                <input type="radio" value="female" name="gender">Female
+            </ul>
         </form>
     </body>
 </html>
