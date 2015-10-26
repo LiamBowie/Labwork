83b488a6daa81c77efd5382533058aaeb50d665d
1206499 <1206499@rgu.ac.uk>
2015-10-26 16:24:51 +0000

    Updated

--- Lab2Act2/HeroResponse.php
+++ Lab2Act2/HeroResponse.php
@@ -1,6 +1,10 @@
 <html>
     <body>
         <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+
         $forename = $_POST["forename"];
         $surname = $_POST["surname"];
         $gender = $_POST["gender"];
