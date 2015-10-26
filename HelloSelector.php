2e850955c5b3a820ab016b4fd6f59a5c1d353be1
1206499 <1206499@rgu.ac.uk>
2015-10-26 14:20:29 +0000

    Added HelloSelector.php & HelloPrinter.php

--- HelloSelector.php
+++ HelloSelector.php
@@ -2,8 +2,10 @@
     <body>
         <p>Select where you would like to go:</p>
 
-        <a href="http://1206499-labwork.azurewebsites.net/HelloPrinter.php?planet="Earth">Earth</a>
-        <a href="http://1206499-labwork.azurewebsites.net/HelloPrinter.php?planet="Mars">Mars</a>
-        <a href="http://1206499-labwork.azurewebsites.net/HelloPrinter.php?planet="Uranus">Uranus</a>
+        <ul>
+            <li><a href="http://1206499-labwork.azurewebsites.net/HelloPrinter.php?planet=Earth">Earth</a></li>
+            <li><a href="http://1206499-labwork.azurewebsites.net/HelloPrinter.php?planet=Mars">Mars</a></li>
+            <li><a href="http://1206499-labwork.azurewebsites.net/HelloPrinter.php?planet=Uranus">Uranus</a></li>
+        </ul>
     </body>
 </html>
