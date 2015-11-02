e25c8378bb6c8826ce4a9e1efd1a73017ca24583
1206499 <1206499@rgu.ac.uk>
2015-11-02 16:01:15 +0000

    Initial Commit

---
+++ Lab3Act2/homepage.php
@@ -0,0 +1,18 @@
+<html>
+    <?php session_start() ?>
+    <body>
+        <?php
+        displayAccessLevelInformation($_SESSION["access_level"]);
+
+        function displayAccessLevelInformation($access_level)
+        {
+            if ($access_level == "standard_user") {
+                echo "<p> you are logged in as a standard user</p>";
+            } elseif ($access_level == "root") {
+                echo "<p>you are currently logged in as a root user</p>";
+                echo "<p>you now have access to additional administrative privelages.</p>";
+            }
+        }
+        ?>
+    </body>
+</html>
---
+++ Lab3Act2/login.php
@@ -0,0 +1,25 @@
+<html>
+    <body>
+        <form name="form" action="#" method="post">
+            <p>
+                <label>Username</label><input type="text" name="user">
+            </p>
+            <p>
+                <label>Password</label><input type="password" name="pass">
+            </p>
+            <input type="submit" value="submit">
+        </form>
+
+        <?php
+        $username = "LiamBowie";
+        $password = "NormalPass123";
+
+        if($_POST["user"] == $username && $_POST["pass"] == $password)
+        {
+            $_SESSION['username'] = $username;
+            $_SESSION['access_level'] = "standard_user";
+            header("Location: homepage.php");
+        }
+        ?>
+    </body>
+</html>
