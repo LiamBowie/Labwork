8b5f53b8100fa38b34f72ffc733c5a38998d2795
1206499 <1206499@rgu.ac.uk>
2015-11-02 14:53:47 +0000

    - Added a Submit button to the form - moved the <?php> tags out of the </form>

--- Lab3Act1/Login.php
+++ Lab3Act1/Login.php
@@ -9,17 +9,18 @@
                 <label>Password</label>
                 <input type="password" name="pass">
             </p>
-
-            <?php
-                $username = "LiamBowie";
-                $password = "IAmBossEh123";
-
-                if($_POST["user"] == $username && $_POST["pass"] == $password)
-                {
-                    setcookie("username", $username);
-                    setcookie("access_level", "standarduser");
-                }
-            ?>
+            <a href="http://1206499-labwork.azurewebsites.net/Lab3Act1/homepage.php"><input type="submit" value=""login"></a>
         </form>
+
+        <?php
+        $username = "LiamBowie";
+        $password = "IAmBossEh123";
+
+        if($_POST["user"] == $username && $_POST["pass"] == $password)
+        {
+            setcookie("username", $username);
+            setcookie("access_level", "standarduser");
+        }
+        ?>
     </body>
 </html>
